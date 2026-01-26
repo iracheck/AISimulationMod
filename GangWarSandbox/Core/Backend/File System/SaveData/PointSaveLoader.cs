f73188fc40d68bc7dd12ff2779b33ca5f28086ed
iracheck <137561004+iracheck@users.noreply.github.com>
2026-01-25 23:53:05 -0500

    Revert "PointSaveData changes"
    This reverts commit 086ee850c49939743e7232ab2c1dc794e1c0fdfd.

--- GangWarSandbox/Core/Backend/File System/SaveData/PointSaveLoader.cs
+++ GangWarSandbox/Core/Backend/File System/SaveData/PointSaveLoader.cs
@@ -1,5 +1,4 @@
 ﻿using GangWarSandbox.Utilities;
-using GTA.Math;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -15,30 +14,14 @@ namespace GangWarSandbox.Core.Backend.File_System.SaveData
         string savePath = ModFiles.SAVEDATA_PATH + "/InfiniteBattle/";
         List<PointSaveData> SavedData;
 
-        GangWarSandbox Instance = GangWarSandbox.Instance;
-
-        /// <summary>
-        /// Create the save data using the information currently within the world space. This is how its going to be used most of the time.
-        /// </summary>
-        /// <returns></returns>
         private PointSaveData CreateDataFromWorld()
         {
             PointSaveData data = new PointSaveData();
 
-            foreach (var capturePoint in Instance.CapturePoints)
+            foreach (var capturePoint in GangWarSandbox.Instance.CapturePoints)
             {
                 if (capturePoint != null) data.CapturePoints.Add(capturePoint.Position);
             }
-
-            foreach (var t in Instance.Teams)
-            {
-                foreach (var pt in t.SpawnPoints)
-                {
-                    data.SpawnPoints[t.TeamIndex].Add(pt);
-                }
-            }
-
-            return data;
         }
 
         public void SaveFromCurrent(PointSaveData data = null)
@@ -61,24 +44,6 @@ namespace GangWarSandbox.Core.Backend.File_System.SaveData
         public void Load(string filePath)
         {
             SavedData = JsonConvert.DeserializeObject<List<PointSaveData>>(filePath);
-        }
-
-        public void InitializeIntoWorld(PointSaveData data)
-        {
-            foreach (var capturePoint in data.CapturePoints)
-            {
-                CapturePoint point = new CapturePoint(capturePoint);
-
-                Instance.CapturePoints.Add(point);
-            }
-
-            foreach (var teamIndex in data.SpawnPoints.Keys)
-            {
-                foreach (var point in data.SpawnPoints[teamIndex])
-                {
-                    Instance.Teams[teamIndex].AddSpawnpoint(point);
-                }
-            }
         }
     }
 }
